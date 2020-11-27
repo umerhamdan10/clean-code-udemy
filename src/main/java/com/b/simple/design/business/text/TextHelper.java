19af2fab5bfba36bce64e4d5ff280b644c8397f9
Umerhamdankhan <umer.hamdan10@gmail.com>
2020-11-27 10:51:52 -1200

    [Clean CODE] remove space

--- src/main/java/com/b/simple/design/business/text/TextHelper.java
+++ src/main/java/com/b/simple/design/business/text/TextHelper.java
@@ -26,7 +26,6 @@ public class TextHelper {
 		 String first2CharacterUpdated = first2Characters.replace("A", "");
 
 		 String restOfTheString = str.substring(2);
-
 		 return first2CharacterUpdated + restOfTheString;
 
 	}
